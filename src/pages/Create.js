c58b178e8a8704fc592cfe511a33c088c8eecc0e
DESKTOP-HOSGHCR\johnc <jrcianc@gmail.com>
2021-07-16 11:53:42 -0700

    first commit

---
+++ src/pages/Create.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import Typography from '@material-ui/core/Typography'
+
+export default function Create() {
+  return (
+    <div>
+      {/* <Typography
+        variant="h1"
+        color="primary"
+      >
+        Create a new note
+      </Typography> */}
+
+      <Typography
+        color="secondary"
+        noWrap
+      >
+        Lorem ipsum etc Lorem ipsum etc Lorem ipsum etc Lorem ipsum etc Lorem ipsum etc
+      </Typography>
+    </div>
+  )
+}

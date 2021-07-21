ffb34759d63a60a65aaabe244cd9bb376b7fece1
DESKTOP-HOSGHCR\johnc <jrcianc@gmail.com>
2021-07-21 11:53:51 -0700

    added app bar, updates to styles

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,32 +1,79 @@
 import { 
     Drawer, makeStyles, Typography, 
-    List, ListItem, ListItemIcon, ListItemText
+    List, ListItem, ListItemIcon, ListItemText, AppBar, Toolbar
     } from '@material-ui/core'
+import { AddCircleOutlineOutlined, SubjectOutlined } from '@material-ui/icons'
 import React from 'react'
+import { useHistory, useLocation } from 'react-router'
+import { format } from 'date-fns'
 
 const drawerWidth = 240
 
-const useStyles = makeStyles({
-    page: {
-        background: '#f9f9f9',
-        width: '100%'
-    },
-    drawer: {
-        width: drawerWidth
-    },
-    drawerPaper: {
-        width: drawerWidth
-    },
-    root: {
-        display: 'flex'
+const useStyles = makeStyles((theme) => {
+    return {
+        page: {
+            background: '#f9f9f9',
+            width: '100%',
+            padding: theme.spacing(3)
+        },
+        drawer: {
+            width: drawerWidth
+        },
+        drawerPaper: {
+            width: drawerWidth
+        },
+        root: {
+            display: 'flex'
+        },
+        active: {
+            background: '#f4f4f4'
+        },
+        title: {
+            padding: theme.spacing(2)
+        },
+        appbar: {
+            width: `calc(100% - ${drawerWidth}px)`
+        },
+        toolbar: theme.mixins.toolbar,
+        date: {
+            flexGrow: 1
+        }
     }
 })
 
 const Layout = ({ children }) => {
     const classes = useStyles()
+    const history = useHistory()
+    const location = useLocation()
+
+    const menuItems = [
+        {
+            text:'My Notes',
+            icon: <SubjectOutlined color="secondary" />,
+            path: '/'
+        },
+        {
+            text:'Create A Note',
+            icon: <AddCircleOutlineOutlined color="secondary" />,
+            path: '/create'
+        }
+    ]
     
     return (
         <div className={classes.root}>
+            <AppBar
+                className={classes.appbar}
+                elevation={0}
+            >
+                <Toolbar>
+                    <Typography className={classes.date}>
+                        Today's date is { format(new Date(), 'MMMM do Y') }.
+                    </Typography>
+                    <Typography>
+                        John
+                    </Typography>
+                </Toolbar>
+            </AppBar>
 
             <Drawer
                 className={classes.drawer}
@@ -35,22 +82,29 @@ const Layout = ({ children }) => {
                 classes={{ paper: classes.drawerPaper}}
             >
                 <div>
-                    <Typography variant="h5">
+                    <Typography variant="h5" className={classes.title}>
                         Notes
                     </Typography>
                 </div>
 
                 <List>
-                    <ListItem>
-                        <ListItemText primary="Hello!" />
-                    </ListItem>
-                    <ListItem>
-                        <ListItemText primary="Hello!" />
-                    </ListItem>
+                    {menuItems.map(item => (
+                        <ListItem
+                            button
+                            key={item.text}
+                            onClick={() => history.push(item.path)}
+                            className={location.pathname == item.path ? classes.active : null}
+                        >
+                            <ListItemIcon>{item.icon}</ListItemIcon>
+                            <ListItemText primary={item.text} />
+                        </ListItem>
+                    ))}
                 </List>
             </Drawer>
 
             <div className={classes.page}>
+                <div className={classes.toolbar}>
+                </div>
                 { children }
             </div>
         </div>
